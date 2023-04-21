67a9f556c4217b43383513b500d4ec50ef4cb16f
Oliver Davies <oliver@oliverdavies.uk>
2023-04-21 12:01:22 +0100

    feat: add an example module with custom tests
    Add a custom module based on my Drupal module template:
    
    https://github.com/opdavies/drupal-module-template

---
+++ web/modules/custom/example/src/Controller/ExamplePageController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\example\Controller;
+
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+final class ExamplePageController {
+
+  use StringTranslationTrait;
+
+  public function __construct(
+    private LoggerChannelFactoryInterface $logger
+  ) {}
+
+  public function __invoke(): array {
+    $this->logger->get('example')->info('Example page viewed.');
+
+    return [
+      '#markup' => $this->t(
+        'This is an example page from the <a href="@url">Drupal Module Template</a>.',
+        ['@url' => 'https://github.com/opdavies/drupal-module-template']
+      ),
+    ];
+  }
+
+}
